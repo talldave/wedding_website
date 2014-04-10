317a6dc201f92f172c2dfe4283f2a6fe40722331
David Bianco <me@davidbianco.net>
2014-04-09 21:16:34 -0700

    first commit

---
+++ wedding_site/app/config_sample.py
@@ -0,0 +1,12 @@
+
+CSRF_ENABLED = True
+SECRET_KEY = '42'
+
+SQLALCHEMY_DATABASE_URI = 'mysql://u:p@h/d'
+
+MAIL_SERVER = 'mail.example.com'
+MAIL_USERNAME = 'addr@example.com'
+MAIL_PASSWORD = 'password?'
+#TESTING = True
+TESTING = False
+
